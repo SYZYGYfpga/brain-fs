44f3e02c4790cb61504f289771aa68210560cd15
Jake Janovetz <jake@janovetz.com>
2018-02-06 09:23:01 -0800

    Update status page style.

--- www/status/index.py
+++ www/status/index.py
@@ -33,8 +33,8 @@ print("""
         <div class="container text-content">
           <div class="row">
             <div class="col-sm-12 mb-md">
-              <div class="table-responsive" style="width:100%;">
-              <table class="table table-datatable" style="border:2px; width:100%;">
+              <div class="col-sm-12 table-responsive">
+              <table class="table table-datatable">
                 <thead>
                   <tr>
                     <th>Parameter</th>
